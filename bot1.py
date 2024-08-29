f0cf177ce31fb3d57841ceab17422bf4d9fa3bdc
Santiago <stgoG1414chile@gmail.com>
2024-08-28 21:56:47 -0400

    Update 28/08 21:56 bot1.py

--- bot1.py
+++ bot1.py
@@ -43,4 +43,4 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
-bot.run("xxxxx")
+bot.run("")
